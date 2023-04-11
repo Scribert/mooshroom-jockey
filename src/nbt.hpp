da7da9f37025fbe537fbb85ed426f1fd4fc37c66
Scribert <95598186+Scribert@users.noreply.github.com>
2023-04-10 19:57:28 -0600

    Update nbt.hpp

--- src/nbt.hpp
+++ src/nbt.hpp
@@ -1,1 +1,26 @@
+#include <cstdint>
+#include <memory>
 
+class NbtTag {
+private:
+	enum class Type : std::int8_t {
+		Unconverted = -1
+		End = 0,
+		Byte = 1,
+		Short = 2,
+		Int = 3,
+		Long = 4,
+		Float = 5,
+		Double = 6,
+		ByteArray = 7,
+		String = 8,
+		List = 9,
+		Compound = 10,
+		IntArray = 11,
+		LongArray = 12
+	}
+	
+	std::size_t size;
+	Type type;
+	std::unique_ptr<std::uint8_t[]> data;
+}
